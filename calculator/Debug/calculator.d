3bb44de77a5b48e3199fb2247a8812f1fc86222b
Sayed Mohsen <48262280+SAmohsen@users.noreply.github.com>
2019-12-29 15:46:53 +0200

    calculator

---
+++ calculator/Debug/calculator.d
@@ -0,0 +1,19 @@
+calculator.o calculator.o: ../calculator.c ../calculator.h \
+  ../common_marcos.h ../micro_config.h ../std_types.h ../KeyPad.h \
+  ../KeyPad_cfg.h ../LCD.h ../LCD_cfg.h
+
+../calculator.h:
+
+../common_marcos.h:
+
+../micro_config.h:
+
+../std_types.h:
+
+../KeyPad.h:
+
+../KeyPad_cfg.h:
+
+../LCD.h:
+
+../LCD_cfg.h:
